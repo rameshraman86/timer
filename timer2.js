ed4ddbc91e2d9f12ca9f323d62f1588b1e75e2d1
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-06 15:39:12 -0400

    completed timer2.js

---
+++ timer2.js
@@ -0,0 +1,35 @@
+const readline = require(`readline`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+const ac = new AbortController();
+const signal = ac.signal;
+
+
+rl.question('Set timer for how many seconds? : ', (answer) => {
+  if (answer === 'b' || answer === 'B') {
+    process.stdout.write('\x07');  //code that beeps
+    rl.close();
+  }
+
+  const timerLengthInSeconds = Number(answer);
+  if (!isNaN(timerLengthInSeconds) && timerLengthInSeconds >= 0) {
+    console.log(`Setting timer for ${timerLengthInSeconds}...`);
+    console.log(`${timerLengthInSeconds} seconds timer started...`);
+
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(`${timerLengthInSeconds} seconds timer over.`);
+    }, Number(timerLengthInSeconds) * 1000);
+    rl.close();
+  }
+});
+
+rl.on('SIGINT', () => {
+  console.log('\nThanks for using me, ciao!');
+  rl.close();
+});
